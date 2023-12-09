72587f2b62d3d7df61695cf83e3a679a0c8bfad3
eugene <eugene>
2023-12-09 10:44:19 +0300

    finish task2

---
+++ task2/script4.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
+
+files = [
+    'annotations.xml',
+    'annotations-2.xml',
+    'annotations-3.xml',
+]
+
+new_path = os.path.join('changes')
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
+
+ch_files = []
+
+for file in files:
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    ch_files.append(shutil.copy2(file, new_path))
+
+TREES = [ET.parse(file) for file in ch_files]
+ROOTS = [tree.getroot() for tree in TREES]
+
+def reverse_ids(root: Element):
+    images = root.findall('.//image')
+    for i, image in enumerate(images):
+        reversed_image_id = image.attrib['id'][::-1]
+        image.attrib['id'] = reversed_image_id
+    return root
+
+def jpg_to_png(root: Element):
+    images = root.findall('.//image')
+    for image in images:
+        image_name = image.attrib['name']
+        old_ext = 'jpg'
+        new_ext = 'png'
+        position = image_name.rfind(old_ext)
+        new_image_name = image_name[:position] + new_ext
+        image.attrib['name'] = new_image_name
+    return root
+
+def left_only_name(root: Element):
+    images = root.findall('.//image')
+    for image in images:
+        only_image_name = image.attrib['name'].split('/')[-1]
+        image.attrib['name'] = only_image_name
+    return root
+
+for root, file in zip(ROOTS, ch_files):
+    reverse_ids(root)
+    jpg_to_png(root)
+    left_only_name(root)
+    with open(file, 'wb') as new_file:
+        tree = ET.ElementTree(root)
+        tree.write(
+            new_file,
+            encoding="utf-8",
+            xml_declaration=True,
+            short_empty_elements=False,
+        )
